565c0a1c940c92247f40996eb83e269196052647
Дмитрий <d03608665@gmail.com>
2025-05-29 00:36:53 +0300

    Homework for 29.05

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -64,52 +64,6 @@
 // В finally напечатай:
 //
 //     “Сумма завершена.”
-
-
-// Проверка логина
-//
-// Условие:
-// Метод Login(string username)
-// Если строка пуста — выбрось ArgumentNullException.
-//     Если длина < 4 — выбрось Exception с сообщением: “Слишком короткий логин”
-
-
-using System.Globalization;
-using System.Xml;
-
-void Login(string username, string password)
-{
-    try
-    {
-        try
-        {
-            if (username.Length==0)
-            {
-                throw new ArgumentException();
-
-            }
-            else if (username.Length<4)
-            {
-               Console.WriteLine("Username is too short");
-            }
-        }
-        
-        catch
-        {
-            Console.WriteLine("Exception occured");
-            throw;
-        }
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine(e.Message);
-    }
-}
-
-
-Login("sss", "sss+");
-
-
 // void SumStrings(string a, string b)
 // {
 //
@@ -128,6 +82,187 @@ Login("sss", "sss+");
 // {
 //     Console.WriteLine("End of program");
 // }
+
+
+
+// Проверка логина
+//
+// Условие:
+// Метод Login(string username)
+// Если строка пуста — выбрось ArgumentNullException.
+//     Если длина < 4 — выбрось Exception с сообщением: “Слишком короткий логин”
+// using System.Globalization;
+// using System.Xml;
+// void Login(string username)
+// {
+//     try
+//     {
+//         try
+//         {
+//             if (username.Length==0)
+//             {
+//                 throw new ArgumentException();
+//
+//             }
+//             else if (username.Length<4)
+//             {
+//                Console.WriteLine("Username is too short");
+//             }
+//         }
+//         
+//         catch
+//         {
+//             Console.WriteLine("Exception occured");
+//             throw;
+//         }
+//     }
+//     catch (Exception e)
+//     {
+//         Console.WriteLine(e.Message);
+//     }
+// }
+// Login("");
+
+
+// Свой throw
+//
+//     Условие:
+// Если число меньше 0, выбрось исключение ArgumentException с сообщением:
+//
+//     “Число не может быть отрицательным!”
+
+
+// try
+// {
+//     try
+//     {
+//         int num = Convert.ToInt32(Console.ReadLine());
+//         if (num < 0)
+//         {
+//             throw new ArgumentException("Negative number");
+//         }
+//
+//         else
+//         {
+//             Console.WriteLine(num);
+//         }
+//
+//     }
+//     catch
+//     {
+//         Console.WriteLine("Exception occured");
+//         throw;
+//     }
+// }
+// catch(Exception ex)
+// {
+//     Console.WriteLine(ex.Message);
+// }
+    
+    
+
+// Чтение из массива
+//
+// Условие:
+// Напиши метод ReadElement(int[] array, int index)
+// Если индекс вне границ массива — перехвати IndexOutOfRangeException
+// и выведи:
+//
+//     “Элемент с таким индексом не найден.”
+//
+// В finally выведи:
+//
+//     “Завершено чтение элемента.”
+
+// void ReadElement(int[] array, int index)
+// {
+//     try
+//     {
+//         try
+//         {
+//
+//             if (index >= array.Length)
+//             {
+//                 throw new IndexOutOfRangeException("Index is out of range.");
+//             }
+//
+//
+//             else
+//             {
+//                 Console.WriteLine(array[index]);
+//             }
+//
+//         }
+//         catch
+//         {
+//             Console.WriteLine("Exception occured");
+//             throw;
+//         }
+//     }
+//     catch (Exception ex)
+//     {
+//         Console.WriteLine(ex.Message);
+//     }
+//     finally
+//     {
+//         Console.WriteLine("End of file");
+//     }
+//
+// }
+// int[] array=new int[10];
+// ReadElement(array,2);
+
+
+
+// Проверка пароля
+//
+// Условие:
+// Метод CheckPassword(string password)
+//     • Если password null или пустой — выбрось ArgumentNullException
+//     • Если длина меньше 8 — выбрось Exception("Слишком короткий пароль")
+//     • Если всё ок — напечатай: “Пароль принят”
+//
+// В finally — “Проверка завершена
+
+// void CheckPassword(string? password)
+// {
+//     try
+//     {
+//          try
+//          {
+//
+//              if (password.Length==0)
+//              {
+//                  throw new ArgumentNullException();
+//              }
+//              
+//              else if (password.Length < 8)
+//              {
+//                  throw new Exception("Too short password");
+//              }
+//              else
+//              {
+//                  Console.WriteLine("Everithing is ok");
+//              }
+//
+//          }
+//          catch
+//          {
+//              Console.WriteLine("Exception occured");
+//              throw;
+//          }
+//     }
+//     catch (Exception ex)
+//     {
+//          Console.WriteLine(ex.Message);
+//     }
+//     finally
+//     {
+//          Console.WriteLine("End of file");
+//     }
+//     
+// }
+// CheckPassword("");
 
 
 
