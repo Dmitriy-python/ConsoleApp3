bc61f5027dfbdca6dd1b7c0ce5cb76d4ef5bd2c6
Vladislav <gogoha.hug.8172@gmail.com>
2025-05-30 14:20:27 +0300

    л

--- ConsoleApp3/obj/Debug/net9.0/ConsoleApp3.AssemblyInfo.cs
+++ ConsoleApp3/obj/Debug/net9.0/ConsoleApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+565c0a1c940c92247f40996eb83e269196052647")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ca4a3f020435a3435affbc982e25235f424fab7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
